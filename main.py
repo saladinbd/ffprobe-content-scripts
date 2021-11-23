7070f35baf7044c4d1b61c12faf8a48cdb5de5cf
Zahedul Alam <zahedul.alam@m2amedia.tv>
2021-11-23 20:30:31 +0600

    working on checking

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ import sys
 from pathlib import Path
 from typing import NamedTuple
 import json
+import logging
 
+logging.root.handlers = []
+logging.basicConfig(format='%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class FFProbeResult(NamedTuple):
     return_code: int
@@ -31,44 +35,68 @@ def ffprobe(file_path, print_format="json") -> FFProbeResult:
                          return_format=print_format,
                          error=result.stderr)
 
+def calculate_duration(content_frames):
+    frames = content_frames.get("frames", [])
+
+    sum_video = {}
+    sum_audio = {}
+    for frame in frames:
+
+        if frame.get("media_type") == "audio":
+            if frame.get("stream_index") not in sum_audio:
+                sum_audio[frame.get("stream_index")] = 0.0
+            sum_audio[frame.get("stream_index")] += float(frame.get("pkt_duration_time", 0.0))
+
+        elif frame.get("media_type") == "video":
+            if frame.get("stream_index") not in sum_video:
+                sum_video[frame.get("stream_index")] = 0.0
+            sum_video[frame.get("stream_index")] += float(frame.get("pkt_duration_time", 0.0))
+
+    return {
+        "audio_frames_total_duration": list(sum_audio.values()),
+        "video_frames_total_duration": list(sum_video.values())
+    }
+
+
+def validate_audio_video_frames_duration(content_metadata):
+    
+    video_duration = content_metadata.get("video_frames_total_duration")[0]
+
+    if round(video_duration, 2) % 1.92 != 0.0:
+        logger.exception(f"total frame duration not divisible by 1.92 [{video_duration}]")
+        return False
+
+    for duration in content_metadata.get("audio_frames_total_duration"):
+        if video_duration != duration:
+            logger.exception(f"Video and Audio duration not sync [{video_duration}, {duration}]")
+            return False
+
+    return True
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='View ffprobe output')
     parser.add_argument('-i', '--input', help='File Name', required=True)
     parser.add_argument('-frm', '--return_format', help='CSV|JSON', required=True)
     args = parser.parse_args()
     if not Path(args.input).is_file():
-        print("could not read file: " + args.input)
+        logger.exception(f"could not read file: {args.input}")
         exit(1)
-    print('File:       {}'.format(args.input))
+    logger.info('File: {}'.format(args.input))
     ffprobe_result = ffprobe(file_path=args.input, print_format=args.return_format)
-    print(ffprobe_result.return_code)
+
     if ffprobe_result.return_code == 0:
         
+        data = json.loads(ffprobe_result.result)
 
         file_name = args.input.split(".")[0]
-        data = json.loads(ffprobe_result.result)
         with open(f"{file_name}.json", 'w+') as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
 
-        frames = data.get("frames", [])
+        result = calculate_duration(data)
+        logger.info(f"calculation result {result}")
 
-        sum_video = {}
-        sum_audio = {}
-        for frame in frames:
-
-            if frame.get("media_type") == "audio":
-                if frame.get("stream_index") not in sum_audio:
-                    sum_audio[frame.get("stream_index")] = 0.0
-                sum_audio[frame.get("stream_index")] += float(frame.get("pkt_duration_time", 0.0))
-
-            elif frame.get("media_type") == "video":
-                if frame.get("stream_index") not in sum_video:
-                    sum_video[frame.get("stream_index")] = 0.0
-                sum_video[frame.get("stream_index")] += float(frame.get("pkt_duration_time", 0.0))
-
-
-        print(f"audio duration: {sum_audio} | video duration: {sum_video}")
+        if validate_audio_video_frames_duration(result):
+            logger.info(f"content is valid: {args.input}")
 
     else:
-        print("ERROR")
-        print(ffprobe_result.error, file=sys.stderr)
+        logger.exception(ffprobe_result.error, file=sys.stderr)
